d12b1da70cce6ef5f4c49742ad93de73253b78d9
Georgi Zhivankin <georgijivankin@gmail.com>
2017-01-13 16:42:18 +0100

    Fix for random number generation helper

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -2,10 +2,11 @@ package helpers
 
 import (
 	"math/rand"
+	"strconv"
 	"time"
 )
 
-// Generate a pseudo-random number consisting of as many numbers as specified (default is 32)
+// Generate a pseudo-random number consisting of as many numbers as passed into the function (default is 32)
 func GenerateRandomNumber(maxNumbers int) int {
 	if maxNumbers == 0 {
 		maxNumbers = 32
@@ -13,12 +14,18 @@ func GenerateRandomNumber(maxNumbers int) int {
 	// Seed the random number generator with unixtime nanoseconds
 	rand.Seed(time.Now().Unix())
 
-	var generatedNumber int
+	var generatedNumber string
 
 	for i := 0; i < maxNumbers; i++ {
 
-		generatedNumber += rand.Intn(9)
+		generatedNumber += strconv.Itoa(rand.Intn(10))
+	}
+	// Convert string back to number
+	number, err := strconv.Atoi(generatedNumber)
+	if err != nil {
+		// Newbie conversion, but should be sufficient for our purposes
+		return rand.Intn(100000)
 	}
 
-	return generatedNumber
+	return number
 }
